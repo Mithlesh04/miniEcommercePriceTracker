34a646569fe4fc67e5b351d7692bafd88eb7602a
Mithlesh04 <Mithlesh04>
2025-07-06 12:42:45 +0530

    removed unused code from main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pandas as pd
 from src.scrapers.scraper import Scraper
-from src.scrapers.sites.FlipkartScraper import FlipkartScraper
-from src.utils.save_page_html import save_page_html
 
 PRODUCT_URL_FILE = './product_urls.txt'
 FILE_TO_SAVE_SCRAPED_DATA = './scraped_prices.csv'
@@ -23,14 +21,6 @@ def on_complete(data: list[dict | None]):
 
 
 if __name__ == "__main__":
-
-    # url =  "https://www.flipkart.com/lenovo-ultra-7-155h-wuxga-oled-intel-core-16-gb-1-tb-ssd-windows-11-home-14imh9-thin-light-laptop/p/itm77304edff3814?pid=COMH3G969EWNAHYG&lid=LSTCOMH3G969EWNAHYGM4NHIP&marketplace=FLIPKART&q=laptop&store=6bo%2Fb5g&srno=s_1_1&otracker=search&otracker1=search&fm=Search&iid=017ca714-4ca1-4264-b8c6-30362a80e6c7.COMH3G969EWNAHYG.SEARCH&ppt=sp&ppn=sp&ssid=8w6qqkoo7k0000001751730561076&qH=312f91285e048e09"
-    # Example usage of FlipkartScraper
-    # flipkart_scraper = FlipkartScraper(url, site_name="Flipkart")
-    # product_details = flipkart_scraper.get_product_details()
-    # print("Product Details:", product_details)
-
-    # save_page_html(url, "<html>Sample HTML content for testing</html>", site_name="Flipkart")
     # initialize the Scraper with the list of URLs and the on_complete callback
     Scraper(url_list, on_complete=on_complete)
     pass
